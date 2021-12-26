9ac186c7cd893dcfb6969e94ae8205026663fd9c
Rocco Bowling <rocco@chimerasw.com>
2021-12-26 10:13:44 -0500

    make using public

--- Sources/Hitch/HalfHitch.swift
+++ Sources/Hitch/HalfHitch.swift
@@ -23,7 +23,7 @@ public struct HalfHitch: CustomStringConvertible, Comparable, Hashable {
     var to: Int
 
     @inlinable @inline(__always)
-    static func using(data: Data, from: Int = 0, to: Int = 0, _ callback: (HalfHitch) -> Void) {
+    public static func using(data: Data, from: Int = 0, to: Int = 0, _ callback: (HalfHitch) -> Void) {
         var data2 = data
         data2.withUnsafeMutableBytes { unsafeRawBufferPointer in
             let unsafeBufferPointer = unsafeRawBufferPointer.bindMemory(to: UInt8.self)
