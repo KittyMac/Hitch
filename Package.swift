d3bfe4b90303653d71f5423c6ec2cbe47b75d7ed
Rocco Bowling <rocco@chimerasw.com>
2025-09-27 14:44:12 -0400

    remove force dynamic library

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Hitch",
     products: [
-        .library(name: "Hitch", type: .dynamic, targets: ["Hitch"]),
+        .library(name: "Hitch", targets: ["Hitch"]),
     ],
     dependencies: [ ],
     targets: [
